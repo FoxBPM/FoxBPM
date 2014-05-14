e66c2589dcfa4aff0997a65e290ced079a7aa732
yangchenhui <84989979@qq.com>
2014-05-14 08:57:27 +0800

    rest框架增加了安全校验

--- modules/foxbpm-rest/src/main/java/org/foxbpm/rest/service/api/model/ProcessDefinitionCollectionResouce.java
+++ modules/foxbpm-rest/src/main/java/org/foxbpm/rest/service/api/model/ProcessDefinitionCollectionResouce.java
@@ -9,58 +9,71 @@ import org.foxbpm.engine.ProcessEngineManagement;
 import org.foxbpm.engine.repository.ProcessDefinition;
 import org.foxbpm.engine.repository.ProcessDefinitionQuery;
 import org.foxbpm.rest.common.api.DataResponse;
+import org.foxbpm.rest.service.application.FoxbpmRestApplication;
 import org.restlet.data.Form;
+import org.restlet.data.Status;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
-public class ProcessDefinitionCollectionResouce extends ServerResource{
+public class ProcessDefinitionCollectionResouce extends ServerResource {
 
-	@Get
-	public DataResponse getDefinitions(){
-		ProcessEngine engine = ProcessEngineManagement.getDefaultProcessEngine();
-		
-		ProcessDefinitionQuery processDefinitionQuery = engine.getModelService().createProcessDefinitionQuery();
-	    Form query = getQuery();
-	    Set<String> names = query.getNames();
-	    
-	    // Populate filter-parameters
-	    if(names.contains("category")) {
-	      processDefinitionQuery.processDefinitionCategory(getQueryParameter("category", query));
-	    }
-	    if(names.contains("categoryLike")) {
-	      processDefinitionQuery.processDefinitionCategoryLike(getQueryParameter("categoryLike", query));
-	    }
-	    if(names.contains("key")) {
-	      processDefinitionQuery.processDefinitionKey(getQueryParameter("key", query));
-	    }
-	    if(names.contains("keyLike")) {
-	      processDefinitionQuery.processDefinitionKeyLike(getQueryParameter("keyLike", query));
-	    }
-	    if(names.contains("name")) {
-	      processDefinitionQuery.processDefinitionName(getQueryParameter("name", query));
-	    }
-	    if(names.contains("nameLike")) {
-	      processDefinitionQuery.processDefinitionNameLike(getQueryParameter("nameLike", query));
-	    }
-	    
-	    List<ProcessDefinition> processDefinitions = processDefinitionQuery.list();
-	    List<ProcessDefinitionResponse> results = new ArrayList<ProcessDefinitionResponse>();
-	    for(ProcessDefinition process : processDefinitions){
-	    	ProcessDefinitionResponse response = new ProcessDefinitionResponse();
-	    	response.setId(process.getId());
-	    	response.setName(process.getName());
-	    	response.setProcessKey(process.getKey());
-	    	results.add(response);
-	    }
-	    DataResponse result = new DataResponse();
-	    result.setData(results);
-	    result.setStart(0);
-	    result.setTotal(processDefinitions.size());
-	    
-	    return result;
-	}
 	
-	protected String getQueryParameter(String name, Form query) {
-	    return query.getFirstValue(name);
+	@Get
+	public DataResponse getDefinitions() {
+		//if(authenticate() == false) return null;
+		ProcessEngine engine = ProcessEngineManagement.getDefaultProcessEngine();
+		ProcessDefinitionQuery processDefinitionQuery = engine.getModelService().createProcessDefinitionQuery();
+		Form query = getQuery();
+		Set<String> names = query.getNames();
+
+		// Populate filter-parameters
+		if (names.contains("category")) {
+			processDefinitionQuery.processDefinitionCategory(getQueryParameter("category", query));
+		}
+		if (names.contains("categoryLike")) {
+			processDefinitionQuery.processDefinitionCategoryLike(getQueryParameter("categoryLike", query));
+		}
+		if (names.contains("key")) {
+			processDefinitionQuery.processDefinitionKey(getQueryParameter("key", query));
+		}
+		if (names.contains("keyLike")) {
+			processDefinitionQuery.processDefinitionKeyLike(getQueryParameter("keyLike", query));
+		}
+		if (names.contains("name")) {
+			processDefinitionQuery.processDefinitionName(getQueryParameter("name", query));
+		}
+		if (names.contains("nameLike")) {
+			processDefinitionQuery.processDefinitionNameLike(getQueryParameter("nameLike", query));
+		}
+
+		List<ProcessDefinition> processDefinitions = processDefinitionQuery.list();
+		List<ProcessDefinitionResponse> results = new ArrayList<ProcessDefinitionResponse>();
+		for (ProcessDefinition process : processDefinitions) {
+			ProcessDefinitionResponse response = new ProcessDefinitionResponse();
+			response.setId(process.getId());
+			response.setName(process.getName());
+			response.setProcessKey(process.getKey());
+			results.add(response);
+		}
+		DataResponse result = new DataResponse();
+		result.setData(results);
+		result.setStart(0);
+		result.setTotal(processDefinitions.size());
+
+		return result;
 	}
+
+	protected String getQueryParameter(String name, Form query) {
+		return query.getFirstValue(name);
+	}
+
+	protected boolean authenticate() {
+		String loggedInUser = ((FoxbpmRestApplication) getApplication()).authenticate(getRequest(), getResponse());
+		if (loggedInUser == null) {
+			setStatus(Status.CLIENT_ERROR_UNAUTHORIZED, "Authentication is required");
+			return false;
+		}
+		return true;
+	}
+
 }
--- modules/foxbpm-rest/src/main/java/org/foxbpm/rest/service/application/FoxbpmRestApplication.java
+++ modules/foxbpm-rest/src/main/java/org/foxbpm/rest/service/application/FoxbpmRestApplication.java
@@ -1,15 +1,65 @@
 package org.foxbpm.rest.service.application;
 
 import org.restlet.Application;
+import org.restlet.Request;
+import org.restlet.Response;
 import org.restlet.Restlet;
+import org.restlet.data.ChallengeScheme;
 import org.restlet.routing.Router;
+import org.restlet.security.ChallengeAuthenticator;
+import org.restlet.security.SecretVerifier;
+import org.restlet.security.Verifier;
 
 public class FoxbpmRestApplication extends Application {
 
+	protected ChallengeAuthenticator authenticator;
+
 	@Override
 	public Restlet createInboundRoot() {
+
+		initializeAuthentication();
+
 		Router router = new Router(getContext());
 		RestServicesInit.attachResources(router);
-		return router;
+		authenticator.setNext(router);
+		return authenticator;
 	}
+
+	public void initializeAuthentication() {
+		Verifier verifier = new SecretVerifier() {
+			@Override
+			public boolean verify(String username, char[] password) throws IllegalArgumentException {
+				System.out.println("***************************shabi******************************************");
+				return true;
+			}
+		};
+		authenticator = new ChallengeAuthenticator(null, true, ChallengeScheme.HTTP_BASIC, "Activiti Realm") {
+
+			@Override
+			protected boolean authenticate(Request request, Response response) {
+
+				// Check if authentication is required if a custom
+				// RestAuthenticator is set
+				if (request.getChallengeResponse() == null) {
+					System.out.println("*********************************");
+					return false;
+				} else {
+					System.out.println("####################################*********************************");
+					boolean authenticated = super.authenticate(request, response);
+					return authenticated;
+				}
+			}
+		};
+		authenticator.setVerifier(verifier);
+	}
+
+	public String authenticate(Request request, Response response) {
+		if (!request.getClientInfo().isAuthenticated()) {
+			authenticator.challenge(response, false);
+			return null;
+		}
+		return request.getClientInfo().getUser().getIdentifier();
+	}
+	
+	
 }
