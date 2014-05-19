f222e6d407ed7da46b57606c4152a3ecdd863d45
yangchenhui <84989979@qq.com>
2014-05-19 14:35:22 +0800

    增加identityLink的持久化

--- modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/persistence/IdentityLinkManager.java
+++ modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/persistence/IdentityLinkManager.java
@@ -29,9 +29,9 @@ import org.foxbpm.engine.impl.entity.IdentityLinkEntity;
  */
 public class IdentityLinkManager extends AbstractManager {
 
+	@SuppressWarnings("unchecked")
 	public List<IdentityLinkEntity> findIdentityLinksByTaskId(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		return (List<IdentityLinkEntity>)getSqlSession().selectListWithRawParameter("selectIdentityLinkByTaskId", id);
 	}
 	
 }
