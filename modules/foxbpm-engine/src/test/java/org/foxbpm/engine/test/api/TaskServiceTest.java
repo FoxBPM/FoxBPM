ced0077a29ecd704b8056a859e447733c65bdc15
yangchenhui <84989979@qq.com>
2014-05-27 20:24:55 +0800

    测试类提交

--- modules/foxbpm-engine/src/test/java/org/foxbpm/engine/test/api/TaskServiceTest.java
+++ modules/foxbpm-engine/src/test/java/org/foxbpm/engine/test/api/TaskServiceTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.foxbpm.engine.impl.entity.TaskEntity;
+import org.foxbpm.engine.impl.identity.Authentication;
+import org.foxbpm.engine.impl.task.command.ExpandTaskCommand;
 import org.foxbpm.engine.runtime.ProcessInstance;
 import org.foxbpm.engine.task.Task;
 import org.foxbpm.engine.task.TaskQuery;
@@ -14,18 +16,27 @@ public class TaskServiceTest extends AbstractFoxBpmTestCase {
 
 	public void testTaskQuery(){
 		
-		
+		Authentication.setAuthenticatedUserId("2222");
 		Map<String, Object> transientVariables=new HashMap<String, Object>();
 		transientVariables.put("value", 10);
 		ProcessInstance processInstance=runtimeService.startProcessInstanceById
-		("FirstFoxbpm:1:916131d2-4598-4e4c-952f-c13c76c77f71","bizkey",transientVariables, null);
+		("process_foxbpm_1:1:ded8ceeb-6d8e-4cd9-b6e1-8e49a25beef7","bizkey",transientVariables, null);
 		
 		
 		Task task=taskService.createTaskQuery().processInstanceId(processInstance.getId()).taskNotEnd().singleResult();
-		taskService.complete(task.getId());
+		
+		ExpandTaskCommand expandCommand = new ExpandTaskCommand();
+		expandCommand.setCommandType("general");
+		expandCommand.setTaskCommandId("HandleCommand_1");
+		expandCommand.setTaskId(task.getId());
+		taskService.expandTaskComplete(expandCommand, null);
 		
 		task=taskService.createTaskQuery().processInstanceId(processInstance.getId()).taskNotEnd().singleResult();
-		taskService.complete(task.getId());
+		ExpandTaskCommand expandCommand2 = new ExpandTaskCommand();
+		expandCommand2.setCommandType("general");
+		expandCommand2.setTaskCommandId("HandleCommand_1");
+		expandCommand2.setTaskId(task.getId());
+		taskService.expandTaskComplete(expandCommand2, null);
 		
 		processInstance=runtimeService.createProcessInstanceQuery().processInstanceId(processInstance.getId()).singleResult();
 		
