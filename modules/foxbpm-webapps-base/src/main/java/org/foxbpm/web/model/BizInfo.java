d08f48d2131fd02e0a932a3b45e1b8a739a0c0cb
maenliang <elma@wisedu.com>
2014-06-09 09:03:10 +0800

    DELETE @Data annotation DELETED BY MAENLIANG AT 2014-06-09

--- modules/foxbpm-webapps-base/src/main/java/org/foxbpm/web/model/BizInfo.java
+++ modules/foxbpm-webapps-base/src/main/java/org/foxbpm/web/model/BizInfo.java
@@ -2,16 +2,38 @@ package org.foxbpm.web.model;
 
 import lombok.Data;
 
-
 /**
  * 业务数据定义
  * 
  * @author MEL
  * @date 2014-06-04
  */
-@Data
 public class BizInfo {
 	private String id = "";
 	private String name = "";
 	private String comment = "";
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
 }
