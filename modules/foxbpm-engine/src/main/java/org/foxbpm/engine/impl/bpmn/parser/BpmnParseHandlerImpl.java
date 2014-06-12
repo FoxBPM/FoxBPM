eb75131a9a7a2de4fe98ff1dfaf1c9a75bfb0aed
yangchenhui <84989979@qq.com>
2014-06-12 14:08:17 +0800

    修改线条转换bug

--- modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/bpmn/parser/BpmnParseHandlerImpl.java
+++ modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/bpmn/parser/BpmnParseHandlerImpl.java
@@ -119,7 +119,7 @@ public class BpmnParseHandlerImpl implements ProcessModelParseHandler {
 				}
 				List<SequenceFlow>  sequenceFlows=((FlowNode) flowElement).getOutgoing();
 				for (SequenceFlow sequenceFlow : sequenceFlows) {
-					processDefinitionBuilder.sequenceFlow(sequenceFlow.getTargetRef().getId());
+					processDefinitionBuilder.sequenceFlow(sequenceFlow.getTargetRef().getId(),sequenceFlow.getId());
 				} 
 				processDefinitionBuilder.endFlowNode();
 			}
