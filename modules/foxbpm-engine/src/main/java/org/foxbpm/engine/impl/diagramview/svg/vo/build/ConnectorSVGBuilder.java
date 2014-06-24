fb9725317e0e9d38df57c24883e252eeabf92a9a
maenliang <elma@wisedu.com>
2014-06-24 14:58:34 +0800

    完善　三次贝塞尔曲线　控制点计算方法

--- modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/diagramview/svg/vo/build/ConnectorSVGBuilder.java
+++ modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/diagramview/svg/vo/build/ConnectorSVGBuilder.java
@@ -56,67 +56,59 @@ public class ConnectorSVGBuilder extends AbstractSVGBuilder {
 	 * 设置线条的拐点信息,拐角划圆形
 	 */
 	public void setWayPoints(List<Point> pointList) {
+		if (pointList == null || pointList.size() == 0) {
+			return;
+		}
 		StringBuffer pathBuffer = new StringBuffer();
+		pathBuffer.append(MOVETO_FLAG)
+				.append(String.valueOf(pointList.get(0).getX()))
+				.append(D_SPACE)
+				.append(String.valueOf(pointList.get(0).getY()));
 		int size = pointList.size();
-		for (int i = 0; i < size; i++) {
+		for (int i = 1; i < size; i++) {
 			Point point = pointList.get(i);
-			// 第二个坐标
-			if (i != 0) {
-				// 判断是否只有两个坐标
-				if (size < 3) {
-					pathBuffer.append(LINETO_FLAG)
-							.append(String.valueOf(point.getX()))
-							.append(D_SPACE)
-							.append(String.valueOf(point.getY()));
-				} else {
-					if (i != size - 1) {
-						// 以当前坐标为中心点
-						Point[] bezalPoints = PointUtils.caclBeralPoints(
-								pointList.get(i - 1), pointList.get(i),
-								pointList.get(i + 1));
-						if (bezalPoints[0].getX() != 0.0f
-								&& bezalPoints[0].getY() != 0.0f) {
-							pathBuffer.append(LINETO_FLAG)
-									.append(bezalPoints[0].getX())
-									.append(D_SPACE)
-									.append(bezalPoints[0].getY())
-									.append(D_SPACE);
-						}
-
-						if (bezalPoints[1].getX() != 0.0f
-								&& bezalPoints[1].getY() != 0.0f
-								&& bezalPoints[2].getX() != 0.0f
-								&& bezalPoints[2].getY() != 0.0f
-								&& bezalPoints[3].getX() != 0.0f
-								&& bezalPoints[3].getY() != 0.0f) {
-							pathBuffer.append(PATHCIRCLE_FLAG)
-									.append(bezalPoints[1].getX())
-									.append(D_SPACE)
-									.append(bezalPoints[1].getY())
-									.append(D_SPACE)
-									.append(bezalPoints[2].getX())
-									.append(D_SPACE)
-									.append(bezalPoints[2].getY())
-									.append(D_SPACE)
-									.append(bezalPoints[3].getX())
-									.append(D_SPACE)
-									.append(bezalPoints[3].getY())
-									.append(D_SPACE);
-						}
-
-						pathBuffer.append(LINETO_FLAG)
-								.append(pointList.get(i + 1).getX())
-								.append(D_SPACE)
-								.append(pointList.get(i + 1).getY());
-					}
-				}
-
-			} else {
-				pathBuffer.append(MOVETO_FLAG)
+			if (size < 3) {
+				// 小于三个断点的，直接画直线
+				pathBuffer.append(LINETO_FLAG)
 						.append(String.valueOf(point.getX())).append(D_SPACE)
 						.append(String.valueOf(point.getY()));
-			}
+			} else {
+				if (i != size - 1) {
+					// 根据三个端点计算 三次贝塞尔曲线的起始点 控制点 终点
+					Point startPoint = pointList.get(i - 1);
+					Point center = point;
+					Point end = pointList.get(i + 1);
+					// 以当前坐标为中心点，数组大小是4，结构依次是 [起始点] [控制点A] [控制点B] [终点]
+					Point[] bezalPoints = PointUtils.caclBeralPoints(
+							startPoint, center, end);
+					if (bezalPoints[0].getX() != 0.0f
+							&& bezalPoints[0].getY() != 0.0f) {
+						pathBuffer.append(LINETO_FLAG)
+								.append(bezalPoints[0].getX()).append(D_SPACE)
+								.append(bezalPoints[0].getY()).append(D_SPACE);
+					}
 
+					if (bezalPoints[1].getX() != 0.0f
+							&& bezalPoints[1].getY() != 0.0f
+							&& bezalPoints[2].getX() != 0.0f
+							&& bezalPoints[2].getY() != 0.0f
+							&& bezalPoints[3].getX() != 0.0f
+							&& bezalPoints[3].getY() != 0.0f) {
+						pathBuffer.append(PATHCIRCLE_FLAG)
+								.append(bezalPoints[1].getX()).append(D_SPACE)
+								.append(bezalPoints[1].getY()).append(D_SPACE)
+								.append(bezalPoints[2].getX()).append(D_SPACE)
+								.append(bezalPoints[2].getY()).append(D_SPACE)
+								.append(bezalPoints[3].getX()).append(D_SPACE)
+								.append(bezalPoints[3].getY()).append(D_SPACE);
+					}
+
+					if (i == size - 2) {
+						pathBuffer.append(LINETO_FLAG).append(end.getX())
+								.append(D_SPACE).append(end.getY());
+					}
+				}
+			}
 		}
 		this.pathVo.setD(pathBuffer.toString());
 	}
