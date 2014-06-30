1188d862aa9b722fa94f784d6d51f90ec8d960ad
maenliang <1154507095@qq.com>
2014-06-30 16:01:38 +0800

    修改连接器自动调度功能 bug

--- modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/expression/ExpressionMgmt.java
+++ modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/expression/ExpressionMgmt.java
@@ -22,51 +22,73 @@ import org.foxbpm.engine.impl.Context;
 import org.foxbpm.engine.impl.entity.ProcessDefinitionEntity;
 import org.foxbpm.kernel.runtime.FlowNodeExecutionContext;
 import org.foxbpm.kernel.runtime.KernelVariableScope;
+import org.foxbpm.kernel.runtime.ListenerExecutionContext;
 
 public class ExpressionMgmt {
+
+	/**
+	 * 
+	 * execute(根据环境变量执行连接器脚本)
+	 * 
+	 * @param scriptText
+	 * @param executionContext
+	 * @return
+	 */
+	public static Object execute(String scriptText,
+			ListenerExecutionContext executionContext) {
+		if (executionContext != null) {
+			setVariable("listenerExecutionContext", executionContext);
+		}
+		return Context.getAbstractScriptLanguageMgmt().execute(scriptText);
+
+	}
 
 	public static Object execute(String scriptText) {
 
 		return Context.getAbstractScriptLanguageMgmt().execute(scriptText);
-		
+
 	}
-	
-	public static Object execute(String scriptText,KernelVariableScope variableScope) {
+
+	public static Object execute(String scriptText,
+			KernelVariableScope variableScope) {
 
 		return Context.getAbstractScriptLanguageMgmt().execute(scriptText);
-		
+
 	}
-	
 
-	
-	
-	public static Object execute(String scriptText, FlowNodeExecutionContext executionContext) {
+	public static Object execute(String scriptText,
+			FlowNodeExecutionContext executionContext) {
 
-		return Context.getAbstractScriptLanguageMgmt().execute(scriptText, executionContext);
-		
+		return Context.getAbstractScriptLanguageMgmt().execute(scriptText,
+				executionContext);
+
 	}
 
 	public static Object getVariable(String variableName) {
-		return Context.getAbstractScriptLanguageMgmt().getVariable(variableName);
-		
-	}
-	
-	public static void setVariable(String variableName, Object variableObj,FlowNodeExecutionContext executionContext) {
+		return Context.getAbstractScriptLanguageMgmt()
+				.getVariable(variableName);
 
-		Context.getAbstractScriptLanguageMgmt().setVariable(variableName,variableObj,executionContext);
-		
 	}
-	
+
+	public static void setVariable(String variableName, Object variableObj,
+			FlowNodeExecutionContext executionContext) {
+
+		Context.getAbstractScriptLanguageMgmt().setVariable(variableName,
+				variableObj, executionContext);
+
+	}
 
 	public static void setVariable(String variableName, Object variableObj) {
 
+		Context.getAbstractScriptLanguageMgmt().setVariable(variableName,
+				variableObj);
 
-		Context.getAbstractScriptLanguageMgmt().setVariable(variableName,variableObj);
-		
 	}
 
-	public static Object execute(String scriptText, ProcessDefinitionEntity processDefinition) {
-		return Context.getAbstractScriptLanguageMgmt().execute(scriptText,processDefinition);
-		
+	public static Object execute(String scriptText,
+			ProcessDefinitionEntity processDefinition) {
+		return Context.getAbstractScriptLanguageMgmt().execute(scriptText,
+				processDefinition);
+
 	}
 }
