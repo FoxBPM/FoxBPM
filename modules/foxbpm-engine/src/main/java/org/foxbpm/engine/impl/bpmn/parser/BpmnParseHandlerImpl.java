4705c06162d1fedddd4787cd5b425f0efb5f2f75
kenshinnet <kenshin.net@gmail.com>
2014-07-02 16:30:08 +0800

    新增了泳道的水平信息

--- modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/bpmn/parser/BpmnParseHandlerImpl.java
+++ modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/bpmn/parser/BpmnParseHandlerImpl.java
@@ -236,6 +236,8 @@ public class BpmnParseHandlerImpl implements ProcessModelParseHandler {
 						lane.setHeight((new Float(bpmnShape.getBounds().getHeight())).intValue());
 						lane.setX((new Float(bpmnShape.getBounds().getX())).intValue());
 						lane.setY((new Float(bpmnShape.getBounds().getY())).intValue());
+						lane.getProperties().put(StyleOption.IsHorizontal,bpmnShape.isIsHorizontal());
+						
 					}
 					
 					Style style=null;
