f1aaa0dd4f0ed7cb2cfe396edd322299df1ae74e
maenliang <1154507095@qq.com>
2014-07-07 09:12:44 +0800

    代码改善，并列if改成else if避免不必要的判断

--- modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/version/FixFlowVersion.java
+++ modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/version/FixFlowVersion.java
@@ -24,103 +24,98 @@ import org.foxbpm.engine.impl.util.StringUtil;
  * 流程版本类
  * 
  * 主版本号.子版本号.修正版本号.编译版本号
- * Major_Version_Number.Minor_Version_Number[.Revision_Number[.Build_Number]]
- * 示例 : 4.7.1.20130531
+ * Major_Version_Number.Minor_Version_Number[.Revision_Number[.Build_Number]] 示例
+ * : 4.7.1.20130531
+ * 
  * @author kenshin
- *
+ * 
  */
 public class FixFlowVersion implements Comparable<FixFlowVersion> {
-	
+
 	private String versionString;
 	private int majorVersionNumber;
 	private int minorVersionNumber;
 	private int revisionNumber;
 	private int buildNumber;
-	
 	private int versionNumber;
 
-
-	public FixFlowVersion(String versionString){
-		
-		if(versionString==null||versionString.equals("")){
+	public FixFlowVersion(String versionString) {
+		if (versionString == null || versionString.equals("")) {
 			throw new FoxBPMException("version不能为空!");
 		}
-		
-		this.versionString=versionString;
-		
-		String[] versionStringTemp=versionString.split("\\.");
-		if(versionStringTemp.length!=4){
-			throw new FoxBPMException("version:"+versionString+"格式不正确!");
+
+		this.versionString = versionString;
+
+		String[] versionStringTemp = versionString.split("\\.");
+		if (versionStringTemp.length != 4) {
+			throw new FoxBPMException("version:" + versionString + "格式不正确!");
 		}
-		
+
 		try {
-			this.majorVersionNumber=StringUtil.getInt(versionStringTemp[0]);
-			this.minorVersionNumber=StringUtil.getInt(versionStringTemp[1]);
-			this.revisionNumber=StringUtil.getInt(versionStringTemp[2]);
-			this.buildNumber=StringUtil.getInt(versionStringTemp[3]);
-			this.versionNumber=StringUtil.getInt(this.majorVersionNumber+this.minorVersionNumber+this.revisionNumber+this.buildNumber);
+			this.majorVersionNumber = StringUtil.getInt(versionStringTemp[0]);
+			this.minorVersionNumber = StringUtil.getInt(versionStringTemp[1]);
+			this.revisionNumber = StringUtil.getInt(versionStringTemp[2]);
+			this.buildNumber = StringUtil.getInt(versionStringTemp[3]);
+			this.versionNumber = StringUtil.getInt(new StringBuffer(this.majorVersionNumber)
+					.append(this.minorVersionNumber).append(this.revisionNumber)
+					.append(this.buildNumber).toString());
 		} catch (Exception e) {
-			throw new FoxBPMException("version:"+versionString+"格式不正确!");
+			throw new FoxBPMException("version:" + versionString + "格式不正确!");
 		}
-		
-		
-		
+
 	}
-	
+
 	/**
 	 * 比较大小
 	 */
 	public int compareTo(FixFlowVersion fixFlowVersion) {
-
-		if(this.versionNumber==fixFlowVersion.getVersionNumber()){
+		int tempVersion = fixFlowVersion.getVersionNumber();
+		if (this.versionNumber == tempVersion) {
 			return 0;
-		}
-		if(this.versionNumber>fixFlowVersion.getVersionNumber()){
+		} else if (this.versionNumber > tempVersion) {
 			return 1;
-		}
-		if(this.versionNumber<fixFlowVersion.getVersionNumber()){
+		} else if (this.versionNumber < tempVersion) {
 			return -1;
 		}
 		return 0;
-		
+
 	}
 
-
-	
-	
-	
 	/**
-	 * 主版本号  [主版本号.子版本号.修正版本号.编译版本号]
-	 * @return 
+	 * 主版本号 [主版本号.子版本号.修正版本号.编译版本号]
+	 * 
+	 * @return
 	 */
-	public int getMajorVersionNumber(){
+	public int getMajorVersionNumber() {
 		return this.majorVersionNumber;
 	}
-	
+
 	/**
-	 * 子版本号  [主版本号.子版本号.修正版本号.编译版本号]
+	 * 子版本号 [主版本号.子版本号.修正版本号.编译版本号]
+	 * 
 	 * @return
 	 */
-	public int getMinorVersionNumber(){
+	public int getMinorVersionNumber() {
 		return this.minorVersionNumber;
 	}
-	
+
 	/**
-	 * 修正版本号  [主版本号.子版本号.修正版本号.编译版本号]
+	 * 修正版本号 [主版本号.子版本号.修正版本号.编译版本号]
+	 * 
 	 * @return
 	 */
-	public int getRevisionNumber(){
+	public int getRevisionNumber() {
 		return this.revisionNumber;
 	}
-	
+
 	/**
-	 * 编译版本号  [主版本号.子版本号.修正版本号.编译版本号]
+	 * 编译版本号 [主版本号.子版本号.修正版本号.编译版本号]
+	 * 
 	 * @return
 	 */
-	public int getBuildNumber(){
+	public int getBuildNumber() {
 		return this.buildNumber;
 	}
-	
 
 	public String getVersionString() {
 		return versionString;
@@ -130,18 +125,12 @@ public class FixFlowVersion implements Comparable<FixFlowVersion> {
 		this.versionString = versionString;
 	}
 
-	
 	public int getVersionNumber() {
 		return versionNumber;
 	}
 
-
 	public void setVersionNumber(int versionNumber) {
 		this.versionNumber = versionNumber;
 	}
-	
-	
-	
-	
 
 }
