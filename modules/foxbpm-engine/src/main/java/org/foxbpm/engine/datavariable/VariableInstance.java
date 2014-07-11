51a20a0457f7c60fecd579119beda70b6442b30d
yangchenhui <84989979@qq.com>
2014-07-11 15:14:55 +0800

    增加变量的相关方法及测试类

--- modules/foxbpm-engine/src/main/java/org/foxbpm/engine/datavariable/VariableInstance.java
+++ modules/foxbpm-engine/src/main/java/org/foxbpm/engine/datavariable/VariableInstance.java
@@ -5,8 +5,25 @@ package org.foxbpm.engine.datavariable;
 
 /**
  * @author kenshin
- *
+ * 
  */
 public interface VariableInstance {
 
+	String getId();
+
+	String getProcessInstanceId();
+
+	String getProcessDefinitionId();
+
+	String getProcessDefinitionKey();
+
+	String getKey();
+
+	String getTaskId();
+
+	String getTokenId();
+
+	String getNodeId();
+
+	Object getValueObject();
 }
