8317dbc8a5891639635f524bd7c4895abc9e01a0
kenshinnet <kenshin.net@gmail.com>
2014-07-30 16:22:11 +0800

    修改了任务命令参数

--- modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/task/cmd/CompleteGeneralTaskCmd.java
+++ modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/task/cmd/CompleteGeneralTaskCmd.java
@@ -35,23 +35,19 @@ public class CompleteGeneralTaskCmd extends AbstractExpandTaskCmd<GeneralTaskCom
 	@Override
 	protected Void execute(CommandContext commandContext, TaskEntity task) {
 
-		// 放置流程实例级别的瞬态变量
+		/** 放置流程实例级别的瞬态变量 */
 		task.setProcessInstanceTransientVariables(this.transientVariables);
-		// 获取任务命令
+		/** 获取任务命令 */
 		TaskCommand taskCommand = getTaskCommand(task);
-		// 获取流程内容执行器
+		/** 获取流程内容执行器 */
 		FlowNodeExecutionContext executionContext = getExecutionContext(task);
-		// 任务命令的执行表达式变量
+		/** 任务命令的执行表达式变量 */
 		taskCommand.getExpressionValue(executionContext);
-		//设置任务处理者
+		/** 设置任务处理者 */
 		task.setAssignee(Authentication.getAuthenticatedUserId());
-		// 设置任务上点击的处理命令
-		task.setCommandId(taskCommand.getId());
-		// 设置任务上点击的处理命令类型
-		task.setCommandType(taskCommand.getTaskCommandType());
-		// 设置任务上点击的处理命令文本
-		task.setCommandMessage(taskCommand.getName());
-		// 处理意见
+		/** 设置任务的处理命令 commandId commandName commandType */
+		task.setTaskCommand(taskCommand);		
+		/** 处理意见 */
 		task.setTaskComment(taskComment);
 
 		task.complete();
