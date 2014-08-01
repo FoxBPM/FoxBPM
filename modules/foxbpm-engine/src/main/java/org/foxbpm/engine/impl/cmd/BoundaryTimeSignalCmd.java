0ee68067cb0927339bff005ae15d1848d0249cb0
maenliang <1154507095@qq.com>
2014-08-01 16:00:42 +0800

    BoundaryTimeSingalCmd类添加包注释

--- modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/cmd/BoundaryTimeSignalCmd.java
+++ modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/cmd/BoundaryTimeSignalCmd.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright 1996-2014 FoxBPM ORG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @author MAENLIANG
+ */
 package org.foxbpm.engine.impl.cmd;
 
 import java.util.Map;
@@ -25,18 +42,14 @@ public class BoundaryTimeSignalCmd implements Command<Void> {
 		this.isCancelActivity=isCancelActivity;
 		this.transientVariables=transientVariables;
 		this.persistenceVariables=persistenceVariables;
-
 	}
 
 	@Override
 	public Void execute(CommandContext commandContext) {
 		
 		FlowNodeExecutionContext executionContext = commandContext.getTokenManager().findTokenById(tokenId);
-		
 		KernelProcessDefinitionImpl processDefinition = executionContext.getProcessDefinition();
-		
 		KernelFlowNodeImpl eventNode = processDefinition.findFlowNode(eventNodeId);
-		
 		
 		if(isCancelActivity){
 			/** 中断边界事件 */
@@ -53,11 +66,7 @@ public class BoundaryTimeSignalCmd implements Command<Void> {
 			FlowNodeExecutionContext childrenExecutionContext=executionContext.createChildrenToken();
 			/** 驱动子令牌从事件节点向下 */
 			childrenExecutionContext.signal(eventNode);
-			
 		}
-		
-		
-		
 		return null;
 	}
 
