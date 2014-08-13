229e2a0ff684efe2f3e9603809c24a4c6d7cea6a
kenshinnet <kenshin.net@gmail.com>
2014-08-13 10:11:50 +0800

    新增了任务命令 转办

---
+++ modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/task/cmd/PendingTaskCmd.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 1996-2014 FoxBPM ORG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @author kenshin
+ */
+package org.foxbpm.engine.impl.task.cmd;
+
+import org.foxbpm.engine.impl.entity.TaskEntity;
+import org.foxbpm.engine.impl.interceptor.CommandContext;
+import org.foxbpm.engine.impl.task.command.PendingTaskCommand;
+
+/**
+ * @author kenshin
+ *
+ */
+public class PendingTaskCmd extends AbstractExpandTaskCmd<PendingTaskCommand, Void>{
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * 转办的用户编号
+	 */
+	protected String pendingUserId;
+	
+	public PendingTaskCmd(PendingTaskCommand abstractCustomExpandTaskCommand) {
+		super(abstractCustomExpandTaskCommand);
+		this.pendingUserId=abstractCustomExpandTaskCommand.getPendingUserId();
+	}
+
+	
+
+	@Override
+	protected Void execute(CommandContext commandContext, TaskEntity task) {
+		
+		
+
+		
+		
+		return null;
+	}
+
+}
