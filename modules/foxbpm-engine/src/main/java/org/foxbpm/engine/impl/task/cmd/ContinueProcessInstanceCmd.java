924120fe52f6a02e1e28073342c4f122923a4e18
kenshinnet <kenshin.net@gmail.com>
2014-08-14 11:08:22 +0800

    新增了 暂停、恢复流程实例方法

---
+++ modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/task/cmd/ContinueProcessInstanceCmd.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.foxbpm.engine.impl.task.cmd;
+
+import org.foxbpm.engine.impl.entity.TaskEntity;
+import org.foxbpm.engine.impl.identity.Authentication;
+import org.foxbpm.engine.impl.interceptor.CommandContext;
+import org.foxbpm.engine.impl.task.command.ContinueProcessInstanceCommand;
+import org.foxbpm.engine.task.TaskCommand;
+import org.foxbpm.kernel.runtime.FlowNodeExecutionContext;
+
+/**
+ * @author kenshin
+ *
+ */
+public class ContinueProcessInstanceCmd extends AbstractExpandTaskCmd<ContinueProcessInstanceCommand, Void> {
+
+	private static final long serialVersionUID = 1L;
+
+	public ContinueProcessInstanceCmd(ContinueProcessInstanceCommand abstractCustomExpandTaskCommand) {
+		super(abstractCustomExpandTaskCommand);
+	}
+
+	@Override
+	protected Void execute(CommandContext commandContext, TaskEntity task) {
+		/** 放置流程实例级别的瞬态变量 */
+		task.setProcessInstanceTransientVariables(this.transientVariables);
+		/** 获取任务命令 */
+		TaskCommand taskCommand = getTaskCommand(task);
+		/** 获取流程内容执行器 */
+		FlowNodeExecutionContext executionContext = getExecutionContext(task);
+		/** 任务命令的执行表达式变量 */
+		taskCommand.getExpressionValue(executionContext);
+		/** 设置任务处理者 */
+		task.setAssignee(Authentication.getAuthenticatedUserId());
+		/** 恢复流程实例 */
+		task.getProcessInstance().continueInstance();
+		
+		return null;
+	}
+
+}
---
+++ modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/task/filter/ContinueProcessInstanceFilter.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.foxbpm.engine.impl.task.filter;
+
+import org.foxbpm.engine.task.Task;
+
+/**
+ * @author kenshin
+ *
+ */
+public class ContinueProcessInstanceFilter  extends AbstractCommandFilter {
+
+	@Override
+	public boolean accept(Task task) {
+	
+
+		if (task == null) {
+			return false;
+		}
+
+		if (!task.isSuspended()) {
+			return false;
+		}
+
+		if (task.hasEnded()) {
+			return false;
+		}
+
+		if (isProcessTracking()) {
+			return false;
+		}
+
+		if (task.getDelegationState() != null) {
+			return false;
+		}
+
+		if (task.getAssignee() != null) {
+			return true;
+		}
+
+		if (isAutoClaim(task)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
