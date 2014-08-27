48aeeb28b737f50814aa1a78669a11f1b8d1abc9
demonrain <demonrain@vip.qq.com>
2014-08-27 16:18:13 +0800

    工作日历项目第一次签入

---
+++ modules/foxbpm-workcalendar/src/main/java/org/foxbpm/calendar/mybatis/FoxbpmCalendarMapper.java
@@ -0,0 +1,17 @@
+package org.foxbpm.calendar.mybatis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.foxbpm.engine.impl.mybatis.FoxbpmMapperConfig;
+
+public class FoxbpmCalendarMapper implements FoxbpmMapperConfig {
+
+	@Override
+	public List<String> getMapperConfig() {
+		List<String> xmlList = new ArrayList<String>();
+		xmlList.add("mybatis/mapping/CalendarType.xml");
+		return null;
+	}
+
+}
---
+++ modules/foxbpm-workcalendar/src/test/java/org/foxbpm/calendar/TestGetAllCalendarType.java
@@ -0,0 +1,7 @@
+package org.foxbpm.calendar;
+
+public class TestGetAllCalendarType {
+	public static void main(String[] args) {
+		
+	}
+}
