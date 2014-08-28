6441d114f6ed8fcbdce8270407d2f0bac60c7967
maenliang <Administrator@WIN-20140815RYD>
2014-08-28 16:53:47 +0800

    表格添加多语言支持，添加cookie保存用户配置状态

--- modules/foxbpm-webapps-portal/src/main/webapp/portal/jsportal/todotasks.js
+++ modules/foxbpm-webapps-portal/src/main/webapp/portal/jsportal/todotasks.js
@@ -89,7 +89,23 @@ var pagefunction = function() {
 				                         columns: [ 0, 3, 4 ]
 				                     }
 				                 ]
-				        }
+				        },
+				        "oLanguage": {
+		                    "sProcessing": "正在加载中......",
+		                    "sLengthMenu": "每页显示 _MENU_ 条记录",
+		                    "sZeroRecords": "对不起，查询不到相关数据！",
+		                    "sEmptyTable": "表中无数据存在！",
+		                    "sInfo": "当前显示 _START_ 到 _END_ 条，共 _TOTAL_ 条记录",
+		                    "sInfoFiltered": "数据表中共为 _MAX_ 条记录",
+		                    "sSearch": "搜索",
+		                    "oPaginate": {
+		                        "sFirst": "首页",
+		                        "sPrevious": "上一页",
+		                        "sNext": "下一页",
+		                        "sLast": "末页"
+		                    }
+				        },
+				        "bStateSave" : true
 
 					});
 };
