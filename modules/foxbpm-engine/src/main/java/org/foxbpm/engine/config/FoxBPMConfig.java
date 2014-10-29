322cf3df6bbd02dd684056aa860a98826e728cad
yangguangftlp <yangguangftlp@163.com>
2014-10-29 11:10:11 +0800

    提交添加类注释

--- modules/foxbpm-engine/src/main/java/org/foxbpm/engine/config/FoxBPMConfig.java
+++ modules/foxbpm-engine/src/main/java/org/foxbpm/engine/config/FoxBPMConfig.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright 1996-2014 FoxBPM ORG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @author yangguangftlp
+ */
 package org.foxbpm.engine.config;
 
 import java.util.List;
@@ -5,7 +22,13 @@ import java.util.List;
 import org.foxbpm.engine.impl.datavariable.DataObjectDefinitionImpl;
 import org.foxbpm.engine.impl.event.EventListenerImpl;
 import org.foxbpm.engine.impl.task.TaskCommandDefinitionImpl;
-
+/**
+ * 
+ * foxbpm配置文件模型
+ * 
+ * @author yangguangftlp
+ * @date 2014年10月29日
+ */
 public class FoxBPMConfig {
 	
 	protected List<TaskCommandDefinitionImpl> taskCommandDefinitions;
@@ -36,31 +59,31 @@ public class FoxBPMConfig {
 		return dataObjectDefinitions;
 	}
 	
-	public void addConfig(FoxBPMConfig other){
-		if(other == null){
+	public void addConfig(FoxBPMConfig other) {
+		if (other == null) {
 			return;
 		}
 		
-		if(other.getTaskCommandDefinitions() != null){
-			if(taskCommandDefinitions == null){
+		if (other.getTaskCommandDefinitions() != null) {
+			if (taskCommandDefinitions == null) {
 				taskCommandDefinitions = other.getTaskCommandDefinitions();
-			}else{
+			} else {
 				taskCommandDefinitions.addAll(other.getTaskCommandDefinitions());
 			}
 		}
 		
-		if(other.getEventListeners() != null){
-			if(eventListeners == null){
+		if (other.getEventListeners() != null) {
+			if (eventListeners == null) {
 				eventListeners = other.getEventListeners();
-			}else{
+			} else {
 				eventListeners.addAll(other.getEventListeners());
 			}
 		}
 		
-		if(other.getDataObjectDefinitions() != null){
-			if(dataObjectDefinitions == null){
+		if (other.getDataObjectDefinitions() != null) {
+			if (dataObjectDefinitions == null) {
 				dataObjectDefinitions = other.getDataObjectDefinitions();
-			}else{
+			} else {
 				dataObjectDefinitions.addAll(other.getDataObjectDefinitions());
 			}
 		}
