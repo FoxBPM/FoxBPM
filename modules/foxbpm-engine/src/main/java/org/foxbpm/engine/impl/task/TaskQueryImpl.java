6e945aea977a7be54b7193fdafa952b6ed7fca05
yangguangftlp <yangguangftlp@163.com>
2015-04-25 16:37:36 +0800

    提交修改任务支持评论提交

--- modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/task/TaskQueryImpl.java
+++ modules/foxbpm-engine/src/main/java/org/foxbpm/engine/impl/task/TaskQueryImpl.java
@@ -36,9 +36,8 @@ import org.foxbpm.engine.task.TaskQuery;
 /**
  * @author kenshin
  */
-public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
-		TaskQuery {
-
+public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements TaskQuery {
+	
 	/**
 	 * 
 	 */
@@ -50,7 +49,7 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 	protected String descriptionLike;
 	protected String assignee;
 	protected String owner;
-	//默认查询所有
+	// 默认查询所有
 	protected int assignedFlag = 2;
 	protected String candidateUser;
 	protected String end;
@@ -80,22 +79,24 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 	protected String initiator;
 	protected String isSuspended;
 	protected String tokenId;
+	protected String taskComment;
+	protected String taskCommentLike;
 	
 	// 查询代理任务时，用来存放原始任务处理人
 	protected String oldAssigneeId;
 	protected List<String> taskTypeList = new ArrayList<String>();
-
+	
 	public TaskQueryImpl() {
 	}
-
+	
 	public TaskQueryImpl(CommandContext commandContext) {
 		super(commandContext);
 	}
-
+	
 	public TaskQueryImpl(CommandExecutor commandExecutor) {
 		super(commandExecutor);
 	}
-
+	
 	public TaskQueryImpl taskId(String taskId) {
 		if (taskId == null) {
 			return this;
@@ -103,17 +104,17 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		this.taskId = taskId;
 		return this;
 	}
-
+	
 	public TaskQuery nodeId(String nodeId) {
 		this.nodeId = nodeId;
 		return this;
 	}
-
+	
 	public TaskQueryImpl taskName(String name) {
 		this.name = name;
 		return this;
 	}
-
+	
 	public TaskQueryImpl taskNameLike(String nameLike) {
 		if (nameLike == null) {
 			return this;
@@ -121,7 +122,20 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		this.nameLike = nameLike;
 		return this;
 	}
-
+	
+	public TaskQueryImpl taskComment(String taskComment) {
+		this.taskComment = taskComment;
+		return this;
+	}
+	
+	public TaskQueryImpl taskCommentLike(String taskCommentLike) {
+		if (taskCommentLike == null) {
+			return this;
+		}
+		this.taskCommentLike = taskCommentLike;
+		return this;
+	}
+	
 	// isAgent
 	/**
 	 * 查询agentId代理给当前用户的所有流程， 如果存在"foxbpm_all_flow" 或者不是代理状态,则返回空的list；
@@ -135,14 +149,13 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		}
 		UserEntity user = Authentication.selectUserByUserId(this.oldAssigneeId);
 		if (user == null) {
-			throw ExceptionUtil.getException("10302002",oldAssigneeId);
+			throw ExceptionUtil.getException("10302002", oldAssigneeId);
 		}
 		List<AgentTo> agentInfo = user.getAgentInfo();
 		Date nowDate = new Date();
 		if (agentInfo != null) {
 			for (AgentTo agent : agentInfo) {
-				if (agent.getAgentFrom().equals(this.agentId)
-						&& (agent.getEndTime().after(nowDate))) {
+				if (agent.getAgentFrom().equals(this.agentId) && (agent.getEndTime().after(nowDate))) {
 					// 如果存在foxbpm_all_flow，则直接清空所有key，并中断循环,返回空List
 					if (agent.getProcessKey().equals(Constant.FOXBPM_ALL_FLOW)) {
 						processKeys.clear();
@@ -154,12 +167,12 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		}
 		return processKeys;
 	}
-
+	
 	public TaskQueryImpl isAgent(boolean isAgent) {
 		this.isAgent = isAgent;
 		return this;
 	}
-
+	
 	/**
 	 * 逻辑： 使用方法
 	 * 
@@ -183,17 +196,17 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		this.agentId = agentId;
 		return this;
 	}
-
+	
 	public TaskQuery isSuspended(boolean isSuspended) {
 		this.isSuspended = String.valueOf(isSuspended);
 		return this;
 	}
-
+	
 	public TaskQuery tokenId(String tokenId) {
 		this.tokenId = tokenId;
 		return this;
 	}
-
+	
 	public TaskQueryImpl businessKey(String businessKey) {
 		if (businessKey == null) {
 			return this;
@@ -201,7 +214,7 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		this.businessKey = businessKey;
 		return this;
 	}
-
+	
 	public TaskQueryImpl businessKeyLike(String businessKey) {
 		if (businessKey == null) {
 			return this;
@@ -209,9 +222,9 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		this.businessKeyLike = businessKey;
 		return this;
 	}
-
+	
 	public TaskQueryImpl addTaskType(String taskInstanceType) {
-
+		
 		if (taskInstanceType == null) {
 			return this;
 		}
@@ -224,7 +237,7 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		this.taskTypeList.add(taskInstanceType);
 		return this;
 	}
-
+	
 	public TaskQueryImpl taskDescription(String description) {
 		if (description == null) {
 			return this;
@@ -232,7 +245,7 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		this.description = description;
 		return this;
 	}
-
+	
 	public TaskQuery taskDescriptionLike(String descriptionLike) {
 		if (descriptionLike == null) {
 			return this;
@@ -240,8 +253,7 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		this.descriptionLike = descriptionLike;
 		return this;
 	}
-
-	 
+	
 	public TaskQuery taskSubject(String subject) {
 		if (subject == null) {
 			return this;
@@ -255,8 +267,6 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		return this;
 	}
 	
-	
-	 
 	public TaskQuery taskSubjectLike(String subjectLike) {
 		if (subjectLike == null) {
 			return this;
@@ -264,7 +274,7 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		this.subjectLike = subjectLike;
 		return this;
 	}
-
+	
 	public TaskQueryImpl taskAssignee(String assignee) {
 		if (assignee == null) {
 			return this;
@@ -275,7 +285,7 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		this.assignee = assignee;
 		return this;
 	}
-
+	
 	public TaskQueryImpl taskOwner(String owner) {
 		if (owner == null) {
 			return this;
@@ -283,7 +293,7 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		this.owner = owner;
 		return this;
 	}
-
+	
 	public TaskQuery taskUnnassigned() {
 		this.assignedFlag = 0;
 		return this;
@@ -298,8 +308,6 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		return this;
 	}
 	
-	
-
 	public TaskQueryImpl taskCandidateUser(String candidateUser) {
 		if (candidateUser == null) {
 			return this;
@@ -310,58 +318,52 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		this.candidateUser = candidateUser;
 		return this;
 	}
-
+	
 	public TaskQueryImpl processInstanceId(String processInstanceId) {
 		this.processInstanceId = processInstanceId;
 		return this;
 	}
-
+	
 	public TaskQueryImpl taskCreatedOn(Date createTime) {
 		this.createTime = createTime;
 		return this;
 	}
-
+	
 	public TaskQuery taskCreatedBefore(Date before) {
 		this.createTimeBefore = before;
 		return this;
 	}
-
+	
 	public TaskQuery taskCreatedAfter(Date after) {
 		this.createTimeAfter = after;
 		return this;
 	}
 	
-	 
 	public TaskQuery taskDueDateOn(Date dueDate) {
 		this.dueDate = dueDate;
 		return this;
 	}
 	
-	 
 	public TaskQuery taskDueDateBefore(Date before) {
 		this.dueDateBefore = before;
 		return this;
 	}
 	
-	 
 	public TaskQuery taskDueDateAfter(Date after) {
 		this.dueDateAfter = after;
 		return this;
 	}
 	
-	 
 	public TaskQuery taskEndTimeOn(Date endTime) {
 		this.endTime = endTime;
 		return this;
 	}
 	
-	 
 	public TaskQuery taskEndTimeBefore(Date before) {
 		this.endTimeBefore = before;
 		return this;
 	}
 	
-	 
 	public TaskQuery taskEndTimeAfter(Date after) {
 		this.endTimeAfter = after;
 		return this;
@@ -371,272 +373,267 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements
 		this.processDefinitionKey = processDefinitionKey;
 		return this;
 	}
-
+	
 	public TaskQuery processDefinitionId(String processDefinitionId) {
 		this.processDefinitionId = processDefinitionId;
 		return this;
 	}
-
+	
 	public TaskQuery processDefinitionName(String processDefinitionName) {
 		this.processDefinitionName = processDefinitionName;
 		return this;
 	}
-
+	
 	public TaskQuery processDefinitionNameLike(String processDefinitionLike) {
 		this.processDefinitionNameLike = processDefinitionLike;
 		return this;
 	}
-
+	
 	public List<GroupEntity> getCandidateGroups() {
 		if (candidateUser != null) {
 			return getGroupsForCandidateUser(candidateUser);
 		}
 		return null;
 	}
-
+	
 	protected List<GroupEntity> getGroupsForCandidateUser(String candidateUser) {
 		return Authentication.selectGroupByUserId(candidateUser);
 	}
-
+	
 	// ordering ////////////////////////////////////////////////////////////////
-
+	
 	public TaskQuery orderByTaskId() {
 		return orderBy(TaskQueryProperty.TASK_ID);
 	}
-
+	
 	public TaskQuery orderByTaskName() {
 		return orderBy(TaskQueryProperty.NAME);
 	}
-
+	
 	public TaskQuery orderByTaskDescription() {
 		return orderBy(TaskQueryProperty.DESCRIPTION);
 	}
-
+	
 	public TaskQuery orderByTaskPriority() {
 		return orderBy(TaskQueryProperty.PRIORITY);
 	}
-
+	
 	public TaskQuery orderByProcessInstanceId() {
 		return orderBy(TaskQueryProperty.PROCESS_INSTANCE_ID);
 	}
-
+	
 	public TaskQuery orderByTaskAssignee() {
 		return orderBy(TaskQueryProperty.ASSIGNEE);
 	}
-
+	
 	public TaskQuery orderByTaskCreateTime() {
 		return orderBy(TaskQueryProperty.CREATE_TIME);
 	}
-
+	
 	public TaskQuery orderByDueDate() {
 		return orderBy(TaskQueryProperty.DUE_DATE);
 	}
-
+	
 	public TaskQuery orderByEndTime() {
 		return orderBy(TaskQueryProperty.END_TIME);
 	}
-
+	
 	// results ////////////////////////////////////////////////////////////////
-
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	
+	@SuppressWarnings({"rawtypes", "unchecked"})
 	public List<Task> executeList(CommandContext commandContext) {
 		// ensureVariablesInitialized();
 		checkQueryOk();
-		return (List) commandContext.getTaskManager().findTasksByQueryCriteria(
-				this);
+		return (List) commandContext.getTaskManager().findTasksByQueryCriteria(this);
 	}
-
+	
 	public long executeCount(CommandContext commandContext) {
 		// ensureVariablesInitialized();
 		checkQueryOk();
-		return commandContext.getTaskManager().findTaskCountByQueryCriteria(
-				this);
+		return commandContext.getTaskManager().findTaskCountByQueryCriteria(this);
 	}
-
+	
 	// getters ////////////////////////////////////////////////////////////////
-
+	
 	public String getName() {
 		return name;
 	}
-
+	
 	public String getNameLike() {
 		return nameLike;
 	}
-
+	
 	public String getAssignee() {
 		return assignee;
 	}
-
+	
 	public String getBusinessKey() {
 		return businessKey;
 	}
-
+	
 	public String getBusinessKeyLike() {
 		return businessKeyLike;
 	}
-
+	
 	public String getOwner() {
 		return owner;
 	}
-
+	
 	public int getUnassigned() {
 		return assignedFlag;
 	}
-
+	
 	public String getCandidateUser() {
 		return candidateUser;
 	}
-
+	
 	public String getProcessInstanceId() {
 		return processInstanceId;
 	}
-
+	
 	public String getTaskId() {
 		return taskId;
 	}
-
+	
 	public String getDescription() {
 		return description;
 	}
-
+	
 	public String getDescriptionLike() {
 		return descriptionLike;
 	}
-
+	
 	public Date getCreateTime() {
 		return createTime;
 	}
-
+	
 	public Date getCreateTimeBefore() {
 		return createTimeBefore;
 	}
-
+	
 	public Date getCreateTimeAfter() {
 		return createTimeAfter;
 	}
-
+	
 	public String getProcessDefinitionKey() {
 		return processDefinitionKey;
 	}
-
+	
 	public String getProcessDefinitionId() {
 		return processDefinitionId;
 	}
-
+	
 	public String getProcessDefinitionName() {
 		return processDefinitionName;
 	}
-
+	
 	public String getProcessDefinitionNameLike() {
 		return processDefinitionNameLike;
 	}
-
+	
 	public List<String> getTaskTypeList() {
 		return taskTypeList;
 	}
-
+	
 	public String getEnd() {
 		return end;
 	}
-
+	
 	public TaskQuery taskIsEnd() {
 		this.end = " not null ";
 		return this;
 	}
-
+	
 	public TaskQuery taskNotEnd() {
 		this.end = " null ";
 		return this;
 	}
-
+	
 	public TaskQuery initiator(String initiator) {
 		this.initiator = initiator;
 		return this;
 	}
-
+	
 	public String getInitiator() {
 		return initiator;
 	}
-
+	
 	public boolean getIsAgent() {
 		return isAgent;
 	}
-
+	
 	public String getAgentId() {
 		return agentId;
 	}
-
+	
 	public String getTokenId() {
 		return tokenId;
 	}
-
+	
 	public String getNodeId() {
 		return nodeId;
 	}
-
+	
 	public String getIsSuspended() {
 		return isSuspended;
 	}
-
+	
 	/* 变量查询 */
 	protected String taskVariableKey;
 	protected String taskVariableValue;
 	protected boolean taskVariableValueIsLike;
-
+	
 	protected String processInstanceVariableKey;
 	protected String processInstanceVariableValue;
 	protected boolean processInstanceVariableValueIsLike;
-
+	
 	public TaskQuery variableData(String variableValue, boolean isLike) {
 		this.taskVariableValue = variableValue;
 		this.taskVariableValueIsLike = isLike;
 		return this;
 	}
-
-	public TaskQuery variableData(String variableKey, String variableValue,
-			boolean isLike) {
+	
+	public TaskQuery variableData(String variableKey, String variableValue, boolean isLike) {
 		this.taskVariableValue = variableValue;
 		this.taskVariableValueIsLike = isLike;
 		this.taskVariableKey = variableKey;
-
+		
 		return this;
 	}
-
-	public TaskQuery processInstanceVariableData(String variableValue,
-			boolean isLike) {
+	
+	public TaskQuery processInstanceVariableData(String variableValue, boolean isLike) {
 		this.processInstanceVariableValue = variableValue;
 		this.processInstanceVariableValueIsLike = isLike;
 		return this;
 	}
-
-	public TaskQuery processInstanceVariableData(String variableKey,
-			String variableValue, boolean isLike) {
+	
+	public TaskQuery processInstanceVariableData(String variableKey, String variableValue, boolean isLike) {
 		this.processInstanceVariableValue = variableValue;
 		this.processInstanceVariableValueIsLike = isLike;
 		this.processInstanceVariableKey = variableKey;
 		return this;
 	}
-
+	
 	public String getTaskVariableKey() {
 		return taskVariableKey;
 	}
-
+	
 	public String getTaskVariableValue() {
 		return taskVariableValue;
 	}
-
+	
 	public boolean isTaskVariableValueIsLike() {
 		return taskVariableValueIsLike;
 	}
-
+	
 	public String getProcessInstanceVariableKey() {
 		return processInstanceVariableKey;
 	}
-
+	
 	public String getProcessInstanceVariableValue() {
 		return processInstanceVariableValue;
 	}
-
+	
 	public boolean isProcessInstanceVariableValueIsLike() {
 		return processInstanceVariableValueIsLike;
 	}
